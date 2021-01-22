854c1667bc844ec191baa17a2bcf01574ab442ec
karagedov <k.karagedov@cft.ru>
2021-01-22 17:10:39 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Uploader extends React.Component {
+  handleChange = (event) => {
+    const { files } = event.target;
+    if (!files.length) {
+      return;
+    }
+
+    const formData = new FormData();
+    const fileData = event.target.files[0];
+    formData.append('image', fileData);
+
+    fetch('http://localhost:8080/upload', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        if (response.status === 'OK') {
+          this.props.onChange(response.data);
+        }
+      });
+  };
+
+  render() {
+    return <input type="file" onChange={this.handleChange} />;
+  }
+}
+
+class App extends React.Component {
+  state = { fileInfo: null };
+
+  onChange = (fileInfo) => {
+    this.setState({ fileInfo });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Uploader onChange={this.onChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
